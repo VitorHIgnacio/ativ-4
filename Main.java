46126e5d44d6d23bb3a8ddbc56dced3fdf7ca7c1
Vitor Ignacio <vitor20120ignacio@gmail.com>
2021-10-16 15:55:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+  //variaveis
+  int primeiroN=25;
+  int segundoN=27;
+  int produto;
+  //CALCULO
+  produto=( primeiroN*segundoN );
+//fim
+  System.out.println("produto "+produto);
+  
+
+  }
+}
